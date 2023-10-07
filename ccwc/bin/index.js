39e04b40c9ce0137da60c16253ab328380ee2af5
chetan gupta <chetangupta123raj@gmail.com>
2023-10-07 08:40:39 +0530

    updated files

--- ccwc/bin/index.js
+++ ccwc/bin/index.js
@@ -4,46 +4,49 @@ const fs = require("fs");
 const yargs = require("yargs");
 
 const options = yargs
-  .usage("Usage: -c | -p <fileLocation>")
-  .option("c", {
-    alias: "fileLocation",
+  .usage("Usage: -c | -l | -w <fileLocation>")
+  .options("c", {
+    alias: "bytes",
     describe: "Enter the location of the file",
     type: "string",
     demandOption: false,
   })
   .options("l", {
-    alias: "fileLocation",
+    alias: "lines",
     describe: "Enter the location of the file",
     type: "string",
     demandOption: false,
   })
   .options("w", {
-    alias: "fileLocation",
+    alias: "words",
     describe: "Enter the location of the file",
     type: "string",
     demandOption: false,
   }).argv;
 
-const data = fs.readFileSync(`${options.fileLocation}`, {
-  encoding: "utf-8",
-});
-
-if (options.c) {
-  const byteSize = (str) => new Blob([str]).size;
-  console.log(`${byteSize(data)} ${options.fileLocation}`);
-}
-
 if (options.l) {
+  const data = fs.readFileSync(`${options.lines}`, {
+    encoding: "utf-8",
+  });
   let count = 0;
   for (let i = 0; i < data.length; i++) {
     if (data[i] == "\n") count++;
   }
-  console.log(`${count} ${options.fileLocation}`);
-}
-
-if (options.w) {
+  console.log(`${count} ${options.l}`);
+} else if (options.c) {
+  const data = fs.readFileSync(`${options.bytes}`, {
+    encoding: "utf-8",
+  });
+  const byteSize = (str) => new Blob([str]).size;
+  console.log(`${byteSize(data)} ${options.c}`);
+} else if (options.w) {
+  const data = fs.readFileSync(`${options.words}`, {
+    encoding: "utf-8",
+  });
   let noOfWords = countWords(data);
-  console.log(`${noOfWords} ${options.fileLocation}`);
+  console.log(`${noOfWords} ${options.w}`);
+} else {
+  console.log("Please provide suitable flag");
 }
 
 function countWords(str) {
